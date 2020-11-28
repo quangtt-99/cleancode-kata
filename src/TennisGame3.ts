548a490de7942df60e218851b447976f676a7191
tran thanh quang <quang.tran290699@gmail.com>
2020-11-29 00:57:36 +0700

    fix: Refactor code tennis game 3

--- src/TennisGame3.ts
+++ src/TennisGame3.ts
@@ -2,35 +2,60 @@ import { TennisGame } from './TennisGame';
 
 
 export class TennisGame3 implements TennisGame {
-  private p2: number = 0;
-  private p1: number = 0;
-  private p1N: string;
-  private p2N: string;
+  private point2: number = 0;
+  private point1: number = 0;
+  private person1Name: string;
+  private person2Name: string;
 
-  constructor(p1N: string, p2N: string) {
-    this.p1N = p1N;
-    this.p2N = p2N;
+  private TITLES = ['Love', 'Fifteen', 'Thirty', 'Forty'];
+
+  constructor(person1Name: string, person2Name: string) {
+    this.person1Name = person1Name;
+    this.person2Name = person2Name;
   }
 
   getScore(): string {
-    let s: string;
-    if (this.p1 < 4 && this.p2 < 4 && !(this.p1 + this.p2 === 6)) {
-      const p: string[] = ['Love', 'Fifteen', 'Thirty', 'Forty'];
-      s = p[this.p1];
-      return (this.p1 === this.p2) ? s + '-All' : s + '-' + p[this.p2];
-    } else {
-      if (this.p1 === this.p2)
-        return 'Deuce';
-      s = this.p1 > this.p2 ? this.p1N : this.p2N;
-      return (((this.p1 - this.p2) * (this.p1 - this.p2)) === 1) ? 'Advantage ' + s : 'Win for ' + s;
+    if (this.isYetDeuce()) return this.getScoreYetDeuce();
+    if (this.isDeuce()) return this.getScoreDeuce();
+    if (this.isAdvantage()) return this.getScoreAdvantage();
+    return this.getScoreKnockout();
+  }
+
+  isYetDeuce(): boolean {
+    return Math.max(this.point1, this.point2) <= 3 && (this.point1 + this.point2) !== 6;
+  }
+
+  isDeuce(): boolean {
+    return this.point1 == this.point2 && this.point1 >= 3;
+  }
+
+  isAdvantage(): boolean {
+    return Math.min(this.point1, this.point2) >= 3 && Math.abs(this.point1 - this.point2) == 1;
+  }
+
+  getScoreYetDeuce(): string {
+    if (this.point1 == this.point2) {
+      return this.TITLES[this.point1] + '-All';
     }
+    return this.TITLES[this.point1] + '-' + this.TITLES[this.point2];
+  }
+
+  getScoreDeuce(): string {
+    return 'Deuce';
+  }
+
+  getScoreAdvantage(): string {
+    return 'Advantage ' + (this.point1 > this.point2 ? this.person1Name : this.person2Name);
+  }
+
+  getScoreKnockout(): string {
+    return 'Win for ' + (this.point1 > this.point2 ? this.person1Name : this.person2Name);
   }
 
   wonPoint(playerName: string): void {
     if (playerName === 'player1')
-      this.p1 += 1;
+      this.point1 += 1;
     else
-      this.p2 += 1;
-
+      this.point2 += 1;
   }
 }
